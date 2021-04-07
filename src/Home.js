1cca77f6f42ef404f500d9488080959b5ffb142a
Ciyu Xu <ciyuxu@gmail.com>
2021-04-07 13:39:52 +0200

    added a simple click event handler

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log(" Yay I see it");
+  }
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={ handleClick }>Click me</button>
     </div>
    );
 }
