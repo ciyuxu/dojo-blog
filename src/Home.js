dd0bf04e9a3fb850cf959c5e4d3ef53346e16f3e
Ciyu Xu <ciyuxu@gmail.com>
2021-04-07 16:12:51 +0200

    added event in the handler, so we can use it later if needed

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,16 @@
 const Home = () => {
-  const handleClick = () => {
-    console.log(" Yay I see it");
+  const handleClick = (e) => {
+    console.log(" Yay I see it", e.target);
   }
 
-  const handleClickAgain = (name) => {
-    console.log ("it is me " + name)
+  const handleClickAgain = (name, e) => {
+    console.log ("it is me " + name, e)
   }
   return ( 
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={ handleClick }>Click me</button>
-      <button onClick={ () => {handleClickAgain("xuxu")} }>Click me again</button>
+      <button onClick={ (e) => {handleClickAgain("xuxu", e)} }>Click me again</button>
     </div>
    );
 }
