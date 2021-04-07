c54a731ee546cb5d348764f4d45beb20d360d12a
Ciyu Xu <ciyuxu@gmail.com>
2021-04-07 16:08:52 +0200

    handling pass-in params in click event

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,15 @@ const Home = () => {
   const handleClick = () => {
     console.log(" Yay I see it");
   }
+
+  const handleClickAgain = (name) => {
+    console.log ("it is me " + name)
+  }
   return ( 
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={ handleClick }>Click me</button>
+      <button onClick={ () => {handleClickAgain("xuxu")} }>Click me again</button>
     </div>
    );
 }
